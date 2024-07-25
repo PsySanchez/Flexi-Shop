a2020d6710a948775350f07c591c497198ccce85
Alex <alex.varonin@gmai.com>
2024-07-25 15:58:20 +0300

    add filter for products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,23 +1,63 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { fetchProducts } from "../../actions/productAction";
+import { fetchProducts, fetchCategories } from "../../actions/productAction";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 import "./Products.css";
 
 export default function Products() {
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products.products);
+  const categories = useSelector((state) => state.products.categories);
 
   useEffect(() => {
+    setFilteredProducts(products);
     if (products.length === 0) {
       dispatch(fetchProducts());
     }
   }, [dispatch, products.length]);
 
+  useEffect(() => {
+    if (!categories.length) dispatch(fetchCategories());
+  }, [dispatch, categories]);
+
+  const changeInputHandler = (event) => {
+    const { name, value } = event.target;
+    if (value) {
+      setFilteredProducts(
+        products.filter((product) => product.category === value)
+      );
+    } else {
+      setFilteredProducts(products);
+    }
+  };
+
   return (
     <div className="products-container">
-      {products.map((product) => (
+      {/* filter products by category */}
+      <div className="col-md-10">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select
+          className="form-select "
+          id="category"
+          name="category"
+          onChange={changeInputHandler}
+        >
+          <option value="">Choose category</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {filteredProducts.map((product) => (
         <Link to={`/product/${product.id}`} key={product.id} className="btn">
           <div className="card" style={{ width: "18rem", height: "100%" }}>
             <img
